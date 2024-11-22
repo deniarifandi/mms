207886dda7d2652c455b9a253f8a5012275e9030
deniarifandi <arifandi.malang@gmail.com>
2024-11-22 14:36:32 +0700

    component build

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,31 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('welcome_message');
+    private $aside;
+    private $footerboot;
+    private $navbar;
+    private $allComp;
+
+    function __construct(){
+
+        $this->aside = view('sidebar');
+        $this->footerboot = view('footerboot');
+        $this->navbar = view('navbar');
+
+        $this->allComp = [
+            'footerboot' => $this->footerboot, 
+            'aside' => $this->aside, 
+            'navbar' => $this->navbar
+        ];
+
+
+    }
+
+
+    public function index()
+    {   
+        echo view('header',$this->allComp);
+        echo view('dashboard');
+        echo view('footer');
     }
 }
