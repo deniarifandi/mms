b975d6f83798900dbf249e8dd67ca3718633140f
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2024-12-19 09:30:07 +0700

    update subjects

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,3 +26,13 @@ $routes->group('/classes',static function ($routes){
 	$routes->post('update/(:num)','ClassesController::update/$1');
 	$routes->delete('delete/(:num)','ClassesController::delete/$1');
 });
+
+$routes->group('/subjects',static function ($routes){
+	$routes->get('/','ClassesController::index');
+	$routes->get('show/(:num)','ClassesController::show/$1');
+	$routes->get('create','ClassesController::create');
+	$routes->post('store','ClassesController::store');
+	$routes->get('edit/(:num)','ClassesController::edit/$1');
+	$routes->post('update/(:num)','ClassesController::update/$1');
+	$routes->delete('delete/(:num)','ClassesController::delete/$1');
+});
---
+++ app/Models/Subjects.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Subjects extends Model
+{
+    protected $table      = 'subjects';
+    protected $primaryKey = 'subject_id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['subject_name', 'updated_at'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
