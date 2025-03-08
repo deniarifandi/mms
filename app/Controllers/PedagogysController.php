37dead9b24f4a47813277897217d4061caed1420
deniarifandi <arifandi.malang@gmail.com>
2025-03-08 11:43:59 +0700

    Update PedagogysController.php

--- app/Controllers/PedagogysController.php
+++ app/Controllers/PedagogysController.php
@@ -159,7 +159,9 @@ class PedagogysController extends BaseController
 
         $path = WRITEPATH . 'uploads/pedagogys/'. $filename;
         if (file_exists($path)) {
-            return $this->response->setContentType(mime_content_type($path))->setBody(file_get_contents($path));
+            //return $this->response->setContentType(mime_content_type($path))->setBody(file_get_contents($path));
+            $data['video_url'] = file_get_contents($path);
+            return view("pedagogys/streaming", $data);
         }
         throw new \CodeIgniter\Exceptions\PageNotFoundException("File not found: $filename");
 
